2974b4eabaa3bca0974414090e39929e35426e97
Tho33 <theo.gregorio@laposte.net>
2023-12-01 22:17:20 +0100

    add observationScore in popup (embarked in trad)

--- app/src/material/ObservationCardHelp.tsx
+++ app/src/material/ObservationCardHelp.tsx
@@ -1,21 +1,36 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-import { MaterialHelpProps } from '@gamepark/react-game'
+import { ItemContext, MaterialHelpProps, useGame, usePlayer, usePlayerId, useRules } from '@gamepark/react-game'
+import { MaterialGame } from '@gamepark/rules-api'
+import { Trek12AmazonieRules } from '@gamepark/trek12-amazonie/Trek12AmazonieRules'
+import { ExplorationCardScores } from '@gamepark/trek12-amazonie/material/ExplorationCard'
+import { LocationType } from '@gamepark/trek12-amazonie/material/LocationType'
+import { MaterialType } from '@gamepark/trek12-amazonie/material/MaterialType'
 import { Trans, useTranslation } from 'react-i18next'
 
 export const ObservationCardHelp = ({item}:MaterialHelpProps)=> {
     const { t } = useTranslation()
     const isHidden = item.location?.rotation
+    const scoreArray = ExplorationCardScores[item.id]
+    const rules = useRules<Trek12AmazonieRules>()
+    const player = usePlayerId()
+
+    const ring = rules?.material(MaterialType.ScoreRing)
+      .location(LocationType.ObservationScores)
+      .locationId(item.location?.x)
+      .player(player).getItem()?.location.x
+
     return <>
         <h2>{t(`observation.card.help.title`)}</h2>
         {isHidden 
         ?
         <p css={textCss} >
-            <Trans defaults="observation.card.hidden.help.text" values={{pointsEarned:item.id}}></Trans>
+            {t(`observation.card.hidden.help.text`)}
         </p>
         :
         <p css={textCss} >
-            <Trans defaults="observation.card.revelead.help.text" values={{pointsEarned:item.id}}></Trans>
+            <Trans defaults="observation.card.revelead.help.text" values={{pointsEarned:scoreArray[ring]}}></Trans>
+            <span>{}</span>
         </p>
         }
     </>
