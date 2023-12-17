d4bfd9933aab319b27cc36842a33e227ab4fbd09
Tho33 <theo.gregorio@laposte.net>
2023-12-17 18:37:21 +0100

    fix crossRemaining text

--- app/src/material/CrossHelp.tsx
+++ app/src/material/CrossHelp.tsx
@@ -2,19 +2,31 @@
 import { css } from '@emotion/react'
 import { ItemContext, MaterialHelpProps, useGame, usePlayer, usePlayerId, useRules } from '@gamepark/react-game'
 import { Trek12AmazonieRules } from '@gamepark/trek12-amazonie/Trek12AmazonieRules'
+import { LocationType } from '@gamepark/trek12-amazonie/material/LocationType'
+import { MaterialType } from '@gamepark/trek12-amazonie/material/MaterialType'
 import { Trans, useTranslation } from 'react-i18next'
 
 export const CrossHelp = ({item}:MaterialHelpProps)=> {
     const { t } = useTranslation()
     const rules = useRules<Trek12AmazonieRules>()
-    const crossesRemaning = item.location?.x ? 3-item.location.x : undefined
+    const player = usePlayerId()
+
+    const crossesMade = rules?.material(MaterialType.Cross)
+        .location(LocationType.OperatorChoice)
+        .player(player)
+        .getItems()
+        .filter(obj => obj.location.id === item.location?.id).length 
+
+    const crossesRemaining = crossesMade !== undefined ? 4-crossesMade : 0
+
+        
 
     return <>
         <h2>{t(`cross.help.title`)}</h2>
         <p css={textCss} >
             {t(`cross.help.text`)}
         </p>
-        <p><Trans defaults="cross.help.remaining" values={{crossesRemaning}}></Trans></p>
+        <p><Trans defaults="cross.help.remaining" values={{crossesRemaining}}></Trans></p>
     </>
 }
 
