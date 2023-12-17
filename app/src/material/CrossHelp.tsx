b10d0fbc42810c73644766430ce413f11ad412ae
Tho33 <theo.gregorio@laposte.net>
2023-12-17 20:19:04 +0100

    Pimp and fix CrossHelp

--- app/src/material/CrossHelp.tsx
+++ app/src/material/CrossHelp.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-import { MaterialHelpProps, usePlayerId, useRules } from '@gamepark/react-game'
+import { MaterialHelpProps, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { getPlayerName } from '@gamepark/trek12-amazonie/Trek12AmazonieOptions'
 import { Trek12AmazonieRules } from '@gamepark/trek12-amazonie/Trek12AmazonieRules'
 import { LocationType } from '@gamepark/trek12-amazonie/material/LocationType'
 import { MaterialType } from '@gamepark/trek12-amazonie/material/MaterialType'
@@ -10,15 +11,18 @@ export const CrossHelp = ({item}:MaterialHelpProps)=> {
     const { t } = useTranslation()
     const rules = useRules<Trek12AmazonieRules>()
     const player = usePlayerId()
+    const opponentName = usePlayerName(item.location?.player)
+
+    
 
     const crossesMade = rules?.material(MaterialType.Cross)
         .location(LocationType.OperatorChoice)
-        .player(player)
+        .player(item.location?.player)
         .getItems()
         .filter(obj => obj.location.id === item.location?.id).length 
 
     const crossesRemaining = crossesMade !== undefined ? 4-crossesMade : 0
-
+    const isThePlayer = item.location?.player === player
         
 
     return <>
@@ -26,7 +30,12 @@ export const CrossHelp = ({item}:MaterialHelpProps)=> {
         <p css={textCss} >
             {t(`cross.help.text`)}
         </p>
-        <p><Trans defaults="cross.help.remaining" values={{crossesRemaining}}></Trans></p>
+        {isThePlayer
+            ? <p><Trans defaults="cross.help.yours.remaining" values={{crossesRemaining}}></Trans></p>
+            : <p><Trans defaults="cross.help.theirs.remaining" values={{crossesRemaining, opponentName}}></Trans></p>
+        }
+        
+
     </>
 }
 
