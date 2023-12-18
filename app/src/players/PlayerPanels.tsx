82f5404b0e16f21c21ae4270904c4128b7ec40e2
Tho33 <theo.gregorio@laposte.net>
2023-12-18 21:00:36 +0100

    add score in player panels

--- app/src/players/PlayerPanels.tsx
+++ app/src/players/PlayerPanels.tsx
@@ -1,14 +1,31 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-import { PlayerPanel, usePlayers } from '@gamepark/react-game'
-import { FC } from 'react'
+import { PlayerPanel, usePlayers, useRules } from '@gamepark/react-game'
+import { FC, HTMLAttributes } from 'react'
+import { faStar } from '@fortawesome/free-solid-svg-icons/faStar'
+import { Trek12AmazonieRules } from '@gamepark/trek12-amazonie/Trek12AmazonieRules'
+import { MaterialType } from '@gamepark/trek12-amazonie/material/MaterialType'
+import { LocationType } from '@gamepark/trek12-amazonie/material/LocationType'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export const PlayerPanels: FC<any> = () => {
+type Trek12AmazoniePlayerPanelProps = {
+} & HTMLAttributes<HTMLDivElement>
+
+export const PlayerPanels: FC<Trek12AmazoniePlayerPanelProps> = ({...props}) => {
   const players = usePlayers({ sortFromMe: true })
+  const rules = useRules<Trek12AmazonieRules>()
+
   return (
     <>
       {players.map((player, index) =>
-        <PlayerPanel key={player.id} playerId={player.id} css={panelPosition(index)}/>
+        <PlayerPanel key={player.id} playerId={player.id} css={panelPosition(index)} {...props}>
+          <div css={indicators}>
+
+            <span><FontAwesomeIcon icon={faStar} css={fontIcon} fill="#28B8CE"/> {rules?.getScore(player.id)}</span>
+            
+
+          </div>
+        </PlayerPanel>
       )}
     </>
   )
@@ -19,4 +36,26 @@ const panelPosition = (index: number) => css`
   top: ${8.5 + index * 14}em;
   width: 28em;
   height: 12em;
+`
+
+const indicators = css`
+  width: 100%;
+  height:4em;
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  bottom: 0.4em;
+  left: 0.8em;
+  flex-wrap: wrap;
+
+  & > span {
+    font-size:2.5em;
+    font-family: 'Rock Salt',cursive;
+  }
+
+`
+
+const fontIcon = css`
+  font-size: 1.2em;
+  color: #28B8CE;
 `
