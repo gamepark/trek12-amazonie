f02e3ca8137d37de55f27d896410ac49bcc70d03
Tho33 <theo.gregorio@laposte.net>
2023-12-27 16:05:03 +0100

    Add Water precision on boxes (maybe ask for the real icon)

--- app/src/locator/description/AreaNodeHelp.tsx
+++ app/src/locator/description/AreaNodeHelp.tsx
@@ -1,6 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import { faSailboat } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { LocationHelpProps, PlayMoveButton, useLegalMoves, usePlayerId} from '@gamepark/react-game'
+import { ForestBasicFieldTypes } from '@gamepark/trek12-amazonie/forests/Forest'
+import { Field } from '@gamepark/trek12-amazonie/material/Field'
 import { useTranslation } from 'react-i18next'
 
 export const AreaNodeHelp = (props: LocationHelpProps) => {
@@ -9,10 +13,12 @@ export const AreaNodeHelp = (props: LocationHelpProps) => {
     const isPlayer = props.location.player === player
     const moves = useLegalMoves()
     const linkedMove = isPlayer && moves.find(move => move.itemType === 7 && props.location.id === move.item.location.id)
+    const isWaterField = ForestBasicFieldTypes[props.location.id] === Field.Water
 
     return <>
         <h2>{t(`area.node.help.title`)}</h2>
         <p css={textCss}>{isPlayer ? t(`area.node.yours.help.text`) : t(`area.node.theirs.help.text`)}</p>
+        { isWaterField && <p css={textCss}> <FontAwesomeIcon icon={faSailboat}/> {t(`area.node.river.help.text`)}</p>}
         { isPlayer && linkedMove !== undefined && <p>{t(`area.node.help.play.text`)}</p>}
          { isPlayer && linkedMove !== undefined &&
             <PlayMoveButton move={linkedMove} onPlay={props.closeDialog}>
