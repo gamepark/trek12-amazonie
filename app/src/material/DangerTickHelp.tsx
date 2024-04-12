ecf0a2028f4c65bbfde6b64463d4503a4a33f858
Romain Fromi <romain.fromi@gmail.com>
2024-04-12 17:13:59 +0200

    Fix NaN in danger tick help dialog

--- app/src/material/DangerTickHelp.tsx
+++ app/src/material/DangerTickHelp.tsx
@@ -1,13 +1,17 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-import { MaterialHelpProps, usePlayerId, usePlayerName } from '@gamepark/react-game'
+import { MaterialHelpProps, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { Score } from '@gamepark/trek12-amazonie/rules/helper/Score'
+import { Trek12AmazonieRules } from '@gamepark/trek12-amazonie/Trek12AmazonieRules'
 import { Trans, useTranslation } from 'react-i18next'
 
 export const DangerTickHelp = ({ item }: MaterialHelpProps) => {
   const { t } = useTranslation()
   const player = usePlayerId()
+  const rules = useRules<Trek12AmazonieRules>()!
   const isThePlayer = item.location?.player === player
   const playerName = usePlayerName(item.location?.player)
+  const tickNumber = new Score(rules.game, item.location!.player!).dangerCount
 
   return <>
     <h2>{t(`danger.tick.help.title`)}</h2>
@@ -16,8 +20,8 @@ export const DangerTickHelp = ({ item }: MaterialHelpProps) => {
     </p>
     <p css={textCss}>
       {isThePlayer
-        ? <Trans defaults="danger.tick.help.yours.score" values={{ tickNumber: +item.quantity! }}></Trans>
-        : <Trans defaults="danger.tick.help.theirs.score" values={{ tickNumber: +item.quantity!, player: playerName }}></Trans>}
+        ? <Trans defaults="danger.tick.help.yours.score" values={{ tickNumber }}></Trans>
+        : <Trans defaults="danger.tick.help.theirs.score" values={{ tickNumber, player: playerName }}></Trans>}
     </p>
   </>
 }
