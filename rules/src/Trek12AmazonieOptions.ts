1defa46b61a0cab61c6d79f2c3463e9db556f1d4
Romain Fromi <romain.fromi@gmail.com>
2025-03-27 17:50:09 +0100

    Require a subscriber for every game played

--- rules/src/Trek12AmazonieOptions.ts
+++ rules/src/Trek12AmazonieOptions.ts
@@ -1,13 +1,9 @@
 import { OptionsSpec } from '@gamepark/rules-api'
-import { TFunction } from 'i18next'
 
 export type PlayerId = number
 export type Trek12AmazonieOptions = {
   players: PlayerId
 }
-export const Trek12AmazonieOptionsSpec: OptionsSpec<Trek12AmazonieOptions> = {}
-
-export function getPlayerName(playerId: number, t: TFunction) {
-  return playerId
+export const Trek12AmazonieOptionsSpec: OptionsSpec<Trek12AmazonieOptions> = {
+  subscriberRequired: true
 }
-
